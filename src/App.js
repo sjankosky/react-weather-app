f5394043d99a76f002b9e27115488fbd58649386
sjankosky <sjankosky@gmail.com>
2021-02-22 19:13:30 -0500

    Added Git and Linkedin links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by <a href="https://www.linkedin.com/in/sarah-jankosky-7a657b59/" target="_blank">Sarah Jankosky</a> and is{" "}
+        <a
+          href="https://github.com/sjankosky/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
+
+export default App;
