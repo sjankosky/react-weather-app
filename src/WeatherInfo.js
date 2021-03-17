6ec5ca7030c4933cd9c4fb4a20f51cf81de73c02
sjankosky <sjankosky@gmail.com>
2021-03-17 17:50:37 -0400

    Create new component to show temperature in both fahrenheit and celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -25,8 +26,7 @@ export default function WeatherInfo(props) {
               className="float-left"
             /> */}
             <div className="float-left">
-              <span className="temperature">{props.data.temperature}</span>
-              <span className="unit">°F</span>
+            <WeatherTemperature fahrenheit={props.data.temperature} />
             </div>
           </div>
         </div>
