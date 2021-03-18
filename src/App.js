42e258ff901651383a524828cbede85ffcebe078
sjankosky <sjankosky@gmail.com>
2021-03-18 18:31:43 -0400

    Added more css formatting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        {/* <h1>Weather App</h1> */}
         <Weather defaultCity="Reston" />
         <footer>
           This project is coded by{" "}
